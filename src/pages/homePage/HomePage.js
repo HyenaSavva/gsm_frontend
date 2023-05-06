0b3a2c649d09d7451286cd6f774c01bf44ca0aba
HyenaSavva <re4meister@gmail.com>
2023-05-06 21:07:56 +0300

    add Contact Us page with modal and some basic animation

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,12 +1,17 @@
-import React from "react";
+import { motion } from "framer-motion";
+
 import Background from "./Background/Background";
 
 const HomePage = ({ children, logged }) => {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
       {children}
       <Background />
-    </>
+    </motion.div>
   );
 };
 
